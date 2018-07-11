598c8a3516ff4f5cf9418293a15cf8cc27eae9ba
Krut Patel <krut@Kruts-MacBook-Pro.local>
2018-07-12 02:19:26 +0530

    Graphs - Added new functions

--- Graphs/Assets/GraphGenerator.cs
+++ Graphs/Assets/GraphGenerator.cs
@@ -8,8 +8,17 @@ public class GraphGenerator : MonoBehaviour {
 
 	[Range(10, 100)]
 	public int resolution = 10;
+	
+	public enum GraphFunctionName {
+		Sine,
+		MultiSine
+	}
+	public GraphFunctionName function;
 
 	private Transform[] points;
+	private static GraphFunction[] functions = {
+		SineFunction, MultiSineFunction
+	};
 
 	void Awake(){
 		float step = 2f / resolution;
@@ -29,11 +38,26 @@ public class GraphGenerator : MonoBehaviour {
 	}
 
 	void Update () {
+		float t = Time.time;
+		GraphFunction f = functions[(int)function];
 		for (int i = 0; i < points.Length; i++) {
 			Transform point = points[i];
 			Vector3 position = point.localPosition;
-			position.y = Mathf.Sin(Mathf.PI * (position.x + Time.time));
+			position.y = f(position.x, t);
 			point.localPosition = position;
 		}
+	}
+	
+	// f(x,t)=sin(π(x+t))
+	static float SineFunction (float x, float t) {
+		return Mathf.Sin(Mathf.PI * (x + t));
+	}
+
+   // f(x,t)= 2*(sin(π(x+t))+sin(2π(x+2*t))/2)/3
+	static float MultiSineFunction (float x, float t) {
+		float y = Mathf.Sin(Mathf.PI * (x + t));
+		y += Mathf.Sin(2f * Mathf.PI * (x + 2f * t)) / 2f;
+		y *= 2f / 3f;
+		return y;
 	}
 }
