789a7f3904a959ac846728736f882ef86ac4616d
Krut Patel <krut@Kruts-MacBook-Pro.local>
2018-07-11 22:26:09 +0530

    Analog Clock

---
+++ AnalogClock/Assets/Clock.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Clock : MonoBehaviour {
+
+	public Transform hoursHand;
+	public Transform minutesHand;
+	public Transform secondsHand;
+
+	private const float degreesPerHour = 30;
+	private const float degreesPerMinute = 6;
+	private const float degreesPerSecond = 6;
+
+	void Update(){
+		hoursHand.rotation = Quaternion.Euler(0, DateTime.Now.Hour * degreesPerHour, 0);
+		minutesHand.rotation = Quaternion.Euler(0, DateTime.Now.Minute * degreesPerMinute, 0);	
+		secondsHand.rotation = Quaternion.Euler(0, DateTime.Now.Second * degreesPerSecond, 0);
+	}
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+Some unity projects I have worked on while learning unity. It also includes some scripts that I use for refrence. 
+
+Here are some links which I refered to, while learning Unity:
+
+1. https://catlikecoding.com/unity/tutorials/
